ea46f2ba4d3a4ade6678105b92eb64bf7e89deed
HormiDev <ivanddiegomegia@gmail.com>
2024-12-04 17:15:31 +0100

    dia4 parte1 ok

--- dia4/dia4.c
+++ dia4/dia4.c
@@ -6,16 +6,45 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:17:07 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/12/04 11:22:35 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/12/04 17:09:38 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../42_Libft/libft.h"
 
+int check_XMAS(t_file *file, int i, int j)
+{
+	int	count;
+	
+	count = 0;
+	if (i > 2 && file->array_content[i - 1][j] == 'M' && file->array_content[i - 2][j] == 'A' && file->array_content[i - 3][j] == 'S')
+		count++;
+	if (i > 2 && ft_strlen_p(file->array_content[i]) - j > 3 && file->array_content[i - 1][j + 1] == 'M' && file->array_content[i - 2][j + 2] == 'A' && file->array_content[i - 3][j + 3] == 'S')
+		count++;
+	if (ft_strlen_p(file->array_content[i]) - j > 3 && file->array_content[i][j + 1] == 'M' && file->array_content[i][j + 2] == 'A' && file->array_content[i][j + 3] == 'S')
+		count++;
+	if (ft_strlen_p(file->array_content[i]) - j > 3 && file->lines-1 - i > 2 && file->array_content[i + 1][j + 1] == 'M' && file->array_content[i + 2][j + 2] == 'A' && file->array_content[i + 3][j + 3] == 'S')
+		count++;
+	if (file->lines-1 - i > 2 && file->array_content[i + 1][j] == 'M' && file->array_content[i + 2][j] == 'A' && file->array_content[i + 3][j] == 'S')
+		count++;
+	if (file->lines-1 - i > 2 && j > 2 && file->array_content[i + 1][j - 1] == 'M' && file->array_content[i + 2][j - 2] == 'A' && file->array_content[i + 3][j - 3] == 'S')
+		count++;
+	if (j > 2 && file->array_content[i][j - 1] == 'M' && file->array_content[i][j - 2] == 'A' && file->array_content[i][j - 3] == 'S')
+		count++;
+	if (i > 2 && j > 2 && file->array_content[i - 1][j - 1] == 'M' && file->array_content[i - 2][j - 2] == 'A' && file->array_content[i - 3][j - 3] == 'S')
+		count++;
+	if (count)
+		ft_printf("X found at line %d, column %d, result +%d\n", i + 1, j + 1, count);
+	return (count);
+}
+
+
 int main(int argc, char **argv)
 {
 	t_file	*file;
-	char	**rectengle_array;
+	int i;
+	int j;
+	int result;
 	
 	if (argc != 2)
 	{
@@ -23,5 +52,23 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	file = ft_create_file_from_filename(argv[1]);
-	rectengle_array = ft_create_str_rectangular_array(file)
+	ft_file_print(file);
+	ft_printf("\n");
+	i = 0;
+	result = 0;
+	while (i < file->lines)
+	{
+		j = 0;
+		while (file->array_content[i][j])
+		{
+			if (file->array_content[i][j] == 'X')
+			{
+				result += check_XMAS(file, i, j);
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_printf("Result: %d\n", result);
+	ft_file_clear(&file);
 }

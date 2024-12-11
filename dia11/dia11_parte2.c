1204c84a84f029aab10011824f6fc069ffbf02db
HormiDev <ivanddiegomegia@gmail.com>
2024-12-11 19:39:48 +0100

    dividido recursivamente

--- dia11/dia11_parte2.c
+++ dia11/dia11_parte2.c
@@ -6,7 +6,7 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 16:31:59 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/12/11 18:49:30 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/12/11 19:38:13 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,37 +57,75 @@ void print_list(t_list_dbl *lst)
 	ft_printf("\n");
 }
 
-int main(int argc, char **argv)
+int root(t_list_dbl *lst_stones, int i, int max_blinkings)
 {
-	char	*str = "9759 0 256219 60 1175776 113 6 92833";
-	char	**split = ft_split(str, ' ');
-	t_list_dbl	*lst_stones = NULL;
-	int		i = 0;
-	
-	if (argc != 3)
-	{
-		ft_printf("usage: %s <stones> <blinks>\n", argv[0]);
-		return (1);
-	}
+	long		result = 0;
+	t_list_dbl *lst_root = NULL;
+	t_list_dbl *tmp;
+	int 		j;
+	int			k;
 
-	while (split[i] != NULL)
-	{
-		ft_list_dbl_addnew_back(&lst_stones, split[i]);
-		i++;
-	}
-	i = 0;
 	ft_printf("blinking %d stones: %d\n", i, ft_list_dbl_size(lst_stones));
 	print_list(lst_stones);
-	while (i < 25)
+	k = 0;
+	while (k < 25 && i < max_blinkings)
 	{
 		
 		blinking(lst_stones);
 		lst_stones = ft_list_dbl_get_first(lst_stones);
 		i++;
+		k++;
 		ft_printf("blinking %d stones: %d\n", i, ft_list_dbl_size(lst_stones));
 	}
-	free(split);
-	ft_printf("result: %d\n", ft_list_dbl_size(lst_stones));
+	if (i < max_blinkings)
+	{
+		tmp = lst_stones;
+		while (tmp != NULL)
+		{
+			j = 0;
+			while (tmp != NULL && j < 8)
+			{
+				ft_list_dbl_addnew_back(&lst_root, ft_strdup_p((char *)tmp->content));
+				tmp = tmp->next;
+				j++;
+			}
+			result += root(lst_root, i, max_blinkings);
+			lst_root = NULL;
+		}
+	}
+	else
+		result = ft_list_dbl_size(lst_stones);
+	ft_printf("result: %ld\n", result);
 	ft_list_dbl_clear(&lst_stones, free);
+	return (result);
+}
+
+int main(int argc, char **argv)
+{
+	char		**split;
+	t_list_dbl	*lst_stones = NULL;
+	int			i;
+	int			blinkings;
+	long		result;
+	
+	if (argc != 3)
+	{
+		ft_printf("usage: %s <stones> <blinks>\n", argv[0]);
+		return (1);
+	};
+	split = ft_split(argv[1], ' ');
+	blinkings = ft_atoi(argv[2]);
+	i = 0;
+	while (split[i] != NULL)
+	{
+		ft_list_dbl_addnew_back(&lst_stones, split[i]);
+		i++;
+	}
+
+	i = 0;
+	result = root(lst_stones, i, blinkings);
+	ft_printf("Final result: %ld\n", result);
+	free(split);
+
 	return (0);
 }
